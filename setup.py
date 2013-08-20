e1959c5f34119c5cc86d918b938d79ff0c28f5db
Bertrand Néron <bneron@pasteur.fr>
2013-08-20 15:21:04 +0200

    add target test in setup.py
    the target test run unit test from the newly build moby_exec_engine
    builded library. the mobyle2.lib project need to be intalled first
    
    the tests can be run from src vesion using directly tests/run_test.py
    
      python setup.py test -vv  run tests using build/lib package
      python tests/run_tests -vv run tests using mobyle_exec package

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+from distutils.core import setup 
+from distutils.dist import Distribution
 from distutils.command.install_egg_info import install_egg_info
 from distutils.command.build import build
 from distutils.versionpredicate import VersionPredicate
 import time
 import sys
 
-class check_and_build( build ):
+class check_and_build(build):
     def run(self):
         chk = True
         for req in require_pyt:
@@ -45,22 +46,77 @@ class check_and_build( build ):
             pass
         return True
 
-
+class test_mobexec_engine(build):
     
+    description = "run the unit tests against the build library"
+    
+    user_options = [ ('verbosity' , 'v' , 'verbosity of outputs', 1)]
+    
+    help_options = []
+    
+
+    def initialize_options(self):
+        build.initialize_options(self)
+        self.verbosity = None
+        
+    def finalize_options(self):
+        if self.verbosity is None:
+            self.verbosity = 0
+        else:
+            self.verbosity = int(self.verbosity)
+    
+    
+    def run(self):
+        """
+        """
+        import os
+        this_dir = os.getcwd()
+
+        # change to the test dir and run the tests
+        os.chdir("tests")
+        sys.path.insert(0, '')
+        import run_tests
+        try:
+            import mobyle.common
+        except ImportError:
+            sys.exit("""The project mobyle2.lib is not installed or not in PYTHONPATH.
+skip tests.""")
+        run_tests.run(self.build_lib, [], verbosity = self.verbosity)
+
+        # change back to the current directory
+        os.chdir(this_dir)
+
+
+
+class UsageDistribution(Distribution):
+    
+    def __init__(self,  attrs=None):
+        Distribution.__init__(self, attrs = attrs)
+        self.common_usage = """\
+Common commands: (see '--help-commands' for more)
+
+  setup.py build      will build the package underneath 'build/'
+  setup.py test       will run the tests on the newly build library
+  setup.py install    will install the package
+"""
+
+
+
 require_pyt = [ 'python (>=2.7, <3.0)' ]
 require_mod = [ 'setproctitle (>=1.1.3)',
                 'daemon (>=1.6)' ]
 
 
-setup(name        = 'mobyle.exec_engine',
-      version     =  time.strftime("%Y-%m-%d"),
-      author      = "Néron Bertrand",
-      author_email = "bneron@pasteur.fr" ,
-      license      = "GPLv3" ,
-      url = "https://github.com/mobyle2/mobyle2.exec_engine",
-      download_url = "https://github.com/mobyle2/mobyle2.exec_engine",
-      description  = "asynchron execution engine that allow Mobyle to submit jobs to DRMs",
-      classifiers = [
+setup( distclass = UsageDistribution,
+       name        = 'mobyle.exec_engine',
+       version     =  time.strftime("%Y-%m-%d"),
+       author      = "Néron Bertrand",
+       author_email = "bneron@pasteur.fr" ,
+       license      = "GPLv3" ,
+       url = "https://github.com/mobyle2/mobyle2.exec_engine",
+       download_url = "https://github.com/mobyle2/mobyle2.exec_engine",
+       description  = "asynchron execution engine that allow Mobyle to submit jobs to DRMs",
+       classifiers = [
                      'License :: GPLv3' ,
                      'Operating System :: POSIX' ,
                      'Programming Language :: Python' ,
@@ -69,5 +125,6 @@ setup(name        = 'mobyle.exec_engine',
       packages    = ['mobyle' , 'mobyle.execution_engine'],
       package_dir = {'': 'mob2exec'},
       scripts     = [ 'mob2exec/bin/mob2execd' ] ,
-      cmdclass= { 'build' : check_and_build }
+      cmdclass= { 'build' : check_and_build,
+                  'test': test_mobexec_engine }
       )
--- tests/run_tests.py
+++ tests/run_tests.py
@@ -15,25 +15,22 @@ import sys
 import unittest
 
 
-if not os.environ['MOBYLE_HOME']:
-    MOBYLE_HOME = os.path.abspath( os.path.join( os.path.dirname( __file__ ), "../", "mob2exec"))
-    os.environ['MOBYLE_HOME'] = MOBYLE_HOME
-MOBYLE_HOME = os.environ['MOBYLE_HOME']
-if (MOBYLE_HOME) not in sys.path:
-    sys.path.append(MOBYLE_HOME)
-  
 
 
-def run(tests, verbosity = 0):
+def run(lib, tests, verbosity = 0):
     """
     run the unit tests and print the results on stderr
     
+    :param lib: the path where is the exec_engine lib
+    :type lib: string
     :param tests: the name of test to run. if tests is empty list, discover recursively tests form this directory.
                   a test is python module with the test_*.py pattern
     :type tests: list of string
     :param verbosity: the verbosity of the output
     :type verbosity: int
     """
+    if lib not in sys.path:
+        sys.path.insert(0, lib)
     if not tests:
         suite = unittest.TestLoader().discover(os.path.dirname(__file__), pattern="test_*.py" ) 
     else:
@@ -52,7 +49,12 @@ def run(tests, verbosity = 0):
 
 
 if __name__ == '__main__':
-
+    
+    if 'MOBYLE_HOME' in os.environ:
+        MOBYLE_HOME = os.environ['MOBYLE_HOME']
+    else:
+        MOBYLE_HOME = os.path.abspath( os.path.join( os.path.dirname( __file__ ), "../", "mob2exec"))
+    
     from argparse import ArgumentParser    
     parser = ArgumentParser()
     parser.add_argument("tests",
@@ -68,4 +70,4 @@ if __name__ == '__main__':
                         )
     
     args = parser.parse_args()
-    run(args.tests , args.verbosity)
+    run(MOBYLE_HOME, args.tests, args.verbosity)

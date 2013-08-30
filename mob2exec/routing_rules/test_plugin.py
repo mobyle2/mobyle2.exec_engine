f99c0abbafe7df6f19f18a42db1b941b080d3385
Bertrand Néron <bneron@pasteur.fr>
2013-08-30 14:03:33 +0200

    add more function rules, for testing framework

--- mob2exec/routing_rules/test_plugin.py
+++ mob2exec/routing_rules/test_plugin.py
@@ -18,6 +18,22 @@ def alacon(job):
 def alacon_bis(zette):
     print zette
     
-def alacon(job):
+def alacon_ter(job):
     print job
 
+@rules.register
+def user_is_local(job):
+    return job.name == 'Filochard'   
+    
+@rules.register
+def job_name_match(job, name = None):
+    return job.name == name
+
+
+from mobyle.common.connection import connection
+from mobyle.common.project import Project
+
+@rules.register
+def project_match(job, name = None):
+    p = connection.Project.find_one({'_id': job.project})
+    return p['name'] == name

cb72b68cef700c37475fb7981f5ef307ca9466cc
Bertrand Néron <bneron@pasteur.fr>
2014-08-08 09:04:23 +0200

    adapt unit tests according to new execengine architecture
    DBManager does not exist anylonger
    each actors deal directly with DB through mongokit object
    JobsTable is not anylonger a manager

--- tests/test_JobsTable.py
+++ tests/test_JobsTable.py
@@ -23,7 +23,7 @@ from mobyle.common.project import Project
 from mobyle.common.job import Status
 from mobyle.common.job import ClJob
 
-from mobyle.execution_engine.engine.jobstable import JobsTable
+from mobyle.execution_engine.engine.monitor import JobsTable
 
 
 class JobsTableTest(unittest.TestCase):
@@ -63,13 +63,10 @@ class JobsTableTest(unittest.TestCase):
         j1.status = Status(Status.BUILDING)
         j1.owner = {'id': self.project.id, 'klass': 'Project'}
         j1.save()
-        jt.put(j1)
+        jt.put(j1.id)
         self.assertEqual(len(jt.jobs()), 1)
-        j2 = jt.get(j1.id)
-        self.assertEqual(j1, j2)
-        self.assertEqual(len(jt.jobs()), 1)
-        j3 = jt.pop(j1.id)
-        self.assertEqual(j1, j3)
+        j3_id = jt.pop(j1.id)
+        self.assertEqual(j1.id, j3_id)
         self.assertEqual(len(jt.jobs()), 0)
          
          
@@ -83,12 +80,12 @@ class JobsTableTest(unittest.TestCase):
             j.status = Status(Status.BUILDING)
             j.owner = {'id': self.project.id, 'klass': 'Project'}
             j.save()
-            jobs_send.append(j)
+            jobs_send.append(j.id)
             #the sensibilty of creation is 1sec minimum
             #so to have a predictable order between job in the list I need to wait for 1 sec 
             time.sleep(1)
-        for j in jobs_send:
-            jt.put(j)
+        for jid in jobs_send:
+            jt.put(jid)
         jobs_recieved = jt.jobs()  
         self.assertEqual(jobs_send, jobs_recieved)
         

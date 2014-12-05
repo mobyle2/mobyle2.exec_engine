33b001a0cebcc2a76601cbf48fb817b0c72019ff
Bertrand Néron <bneron@pasteur.fr>
2014-12-05 17:30:00 +0100

    draft of get_status implementation
    remove the mock and implement a real method to get the status of a job
    implement also methods to get the results when a job is finished

--- mob2exec/mobyle/execution_engine/engine/status_actor.py
+++ mob2exec/mobyle/execution_engine/engine/status_actor.py
@@ -9,15 +9,19 @@
 # @license: GPLv3
 #===============================================================================
 
+import os.path
 import logging.config
 import setproctitle
+import glob
 
+from mobyle.common.data import RefData, ListData
 from mobyle.common.job import Status
 from mobyle.common.error import InternalError
+from mobyle.execution_engine.evaluator import Evaluator, JobLogger
+
 from .actor import Actor
 
 ####### BOUCHON ##########
-import random
 from datetime import datetime
 ##########################
         
@@ -40,7 +44,7 @@ class StatusActor(Actor):
         setproctitle.setproctitle('mob2_status')
         
         logging.config.dictConfig(self._log_conf)
-        self._log = logging.getLogger( __name__ ) 
+        self._log = logging.getLogger(__name__) 
         
         job = self.get_job()
         old_status = job.status.state
@@ -50,27 +54,78 @@ class StatusActor(Actor):
         exec_system_id = str(job.execution_system_id)
         exec_system = self.get_execution_system(exec_system_id)
         
-        self._log.debug( "{0} job {1} was submit with exec sytem {2} and have n° {3}".format(self._name, 
-                                                                                             self.job_id,
-                                                                                             exec_system.name,
-                                                                                             job.execution_job_no))
-        ####################### BOUCHON ###############################
-        if old_status == Status.SUBMITTED:
-            job.status.state = (Status.RUNNING, Status.PENDING, Status.HOLD)[random.randint(0, 2)]
-        elif old_status == Status.PENDING:
-            job.status.state = (Status.PENDING, Status.RUNNING, Status.HOLD, Status.FINISHED, Status.ERROR, Status.KILLED)[random.randint(0, 4)]
-        elif old_status == Status.HOLD:
-            job.status.state = (Status.RUNNING,  Status.HOLD, Status.FINISHED, Status.ERROR, Status.KILLED)[random.randint(0, 3)]
-        elif old_status == Status.RUNNING:
-            job.status.state = (Status.RUNNING,  Status.HOLD, Status.FINISHED, Status.ERROR, Status.KILLED)[random.randint(0, 3)]
-        if job.status.is_ended():
-            job.end_time = datetime.now()
-        ####################### FIN BOUCHON #########################################
-        self._log.debug("{0} set job {1} status {2} to {3}".format(self._name, 
-                                                                   self.job_id, 
-                                                                   Status.UPDATING, 
-                                                                   job.status.state))
+        new_status = exec_system.get_status(job)
+        if old_status != new_status:
+            
+            if new_status.is_ended():
+                job.end_time = datetime.now()
+            if new_status == Status.FINISHED:
+                self.get_results(job)
+            job.status.state =  new_status   
+            job.save()
         job.save()
         self._log.debug("{0} exiting".format(self._name))
         
         
+    def get_results(self, job):
+        """
+        :todo: we assume we have only **ONE** file. to take in account directory we need to recode.  
+        """
+        program = self.job.service
+        evaluator = Evaluator(job, job.log_file_name)
+        with JobLogger(job.log_file_name) as job_log:
+            job_log.debug('###################\n# retrieve results #\n###################')
+            for parameter in program.outputs_list():
+                job_log.debug("------output parameter {0} ------".format(parameter.name))
+                vdef_data = parameter.default_value
+                vdef = self._pre_process_data(vdef_data)
+                evaluator['vdef'] = vdef
+                job_log.debug("vdef = {0}".format(vdef))
+                value = evaluator[parameter.name]
+                evaluator['value'] = value
+                job_log.debug("value = {0}".format(value))
+                preconds = parameter.preconds
+                all_preconds_true = self.eval_precond(preconds, job_log)    
+                if not all_preconds_true :
+                    job_log.debug("all preconds are not True: next parameter")
+                    continue #next parameter
+                
+                filenames = parameter.filenames()
+                job_log.debug("filenames = {0}".format(filenames))
+                unix_mask = None
+                if filenames:
+                    try:
+                        unix_mask = eval(filenames, evaluator)
+                        job_log.debug("unix_mask = {0}".format(unix_mask))
+                    except Exception as err:
+                        msg = "ERROR during evaluation of program {0}: parameter {1} : filenames {2} err {3}".format(program['name'],
+                                                                                                                     parameter.name,
+                                                                                                                     filenames,
+                                                                                                                     err)
+                        self._log.critical(msg, exc_info = True)
+                        job_log.error(msg)
+                        raise InternalError(msg)
+                if unix_mask:
+                    result_files = glob.glob(unix_mask)
+                    job_log.debug("mask match = {0}".format(result_files))
+                    if result_files:
+                        if len(result_files) == 1:
+                            data = RefData()
+                            data['type'] = parameter.type
+                            data['path'] = result_files[0]
+                            data['size'] = os.path.getsize(result_files[0])
+                        else:
+                            data = ListData()
+                            list_of_data = []
+                            for one_file in result_files:
+                                data = RefData()
+                                data['type'] = parameter.type
+                                data['path'] = one_file[0]
+                                data['size'] = os.path.getsize(one_file[0])
+                                list_of_data.append(data)
+                            data['value'] = list_of_data
+                        job['outputs'][parameter.name] = data
+                    else:
+                        continue
+           
+            

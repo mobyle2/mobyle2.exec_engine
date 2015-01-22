b1dc472083c470d34674ae8a4e026a1034cbe333
Bertrand Néron <bneron@pasteur.fr>
2015-01-22 11:40:48 +0100

    replace docker script construction by "template" and format

--- mob2exec/mobyle/execution_engine/engine/docker.py
+++ mob2exec/mobyle/execution_engine/engine/docker.py
@@ -2,70 +2,81 @@ import os
 from mobyle.common.config import Config
 
 class DockerContainer(object):
-  '''
-  Manage Docker container commands
-  '''
-
-  def __init__(self, env_vars=None, volumes=None):
     '''
-    Define a container
-
-    :param env_vars: List of env variables to share in container
-    :type env: dict
-    :param volumes: list of volumes to mount in addition to job directory
-    :type volumes: list
-    :param sudo: Use sudo
-    :type sudo: bool
+    Manage Docker container commands
     '''
-    self.env_vars = env_vars
-    self.volumes = volumes
-    cfg = Config.config()
-    if cfg.get("app:main", "use_sudo") and cfg.get("app:main", "use_sudo") == 'true':
-      self.sudo = "sudo "
-    else:
-      self.sudo = ""
+    
+    def __init__(self, env_vars=None, volumes=None):
+        '''
+        Define a container
+
+        :param env_vars: List of env variables to share in container
+        :type env: dict
+        :param volumes: list of volumes to mount in addition to job directory
+        :type volumes: list
+        '''
+        self.env_vars = env_vars
+        self.volumes = volumes
+        cfg = Config.config()
+        if cfg.get("app:main", "use_sudo") and cfg.get("app:main", "use_sudo") == 'true':
+            self.sudo = "sudo "
+        else:
+            self.sudo = ""
+            
+        ###### BOUCHON ####
+        #a mettre dans la config general avec une valeur par default ?    
+        self.group_mobyle = "mobyle"
+        self.user_mobyle = "mobyle"
 
 
-  def build_pull_command(self, job):
-    '''
-    Builds the pull command
+    def build_pull_command(self, job):
+        '''
+        Builds the pull command
 
-    :param job: Current job
-    :type job: Job
-    '''
-    container = job.service['containers'][0]
-    cmd = self.sudo + 'docker pull '+container['url']+' > .'+container['type']+'.stdout'
-    return cmd
+        :param job: Current job
+        :type job: Job
+        '''
+        container = job.service['containers'][0]
+        cmd = "{sudo} docker pull {container_pull} > .{container_type}.stdout".format(sudo = self.sudo,
+                                                                                      container_pull = container['url'],
+                                                                                      container_type = container['type'])
+        return cmd
 
 
 
-  def build_run_command(self, job):
-    '''
-    Builds the execution command
+    def build_run_command(self, job):
+        '''
+        Builds the execution command
 
-    :param job: Current job
-    :type job: Job
-    '''
-    cmd = ''
-    # Get user and group
-    cmd += 'uid=`id -u`'+"\n"
-    cmd += 'gid=`id -g`'+"\n"
-    container = job.service['containers'][0]
-    cmd += self.sudo + 'docker run '
-    cmd += ' --rm'  #For deletion after execution for cleanup
-    cmd += ' -w '+job['_dir'] # Workdir
-    cmd += ' -v '+job['_dir']+':'+job['_dir'] #Mount job directory as a shared directory
-    if self.volumes:
-      for vol in self.volumes:
-        cmd += ' -v '+vol+':'+vol
-    if self.env_vars:
-      for key, value in self.env_vars.iteritems():
-        cmd += ' -e "'+key+'='+value+'"'
-    cmd += ' '+container['id'] # Image Id
-    cmd += ' bash -c "'
-    # Create user and group
-    cmd += 'groupadd --gid $gid mobyle && useradd --uid $uid --gid $gid mobyle;'
-    # Execute command in container
-    cmd += '/bin/sh '+'.'+job.service['containers'][0]['type']+'_job_script;'
-    cmd += 'chown -R $uid:$gid '+job['_dir']+'"' # Reset file access rights
-    return cmd
+        :param job: Current job
+        :type job: Job
+        '''
+        
+        container = job.service['containers'][0]
+        
+        volumes = ''
+        if self.volumes:
+            for vol in self.volumes:
+                volumes += ' -v {0}:{0}'.format(vol)
+        
+        env = ''
+        if self.env_vars:
+            for key, value in self.env_vars.iteritems():
+                env = ' -e "{0}={1}"'.format(key, value)
+                
+        cmd = '''uid={uid}
+gid={gid}
+{sudo} docker run --rm -w {job_dir} -v {job_dir}:{job_dir} {volumes}{env} {container_id} \ 
+bash -c "groupadd --gid {gid} {group_mobyle} && useradd --uid {uid} --gid {gid} {user_mobyle} \
+/bin/sh .{container_type}_job_script \
+chown -R {uid}:{gid} {job_dir}"'''.format(uid = os.getuid(),
+                                          gid = os.getgid(),
+                                          job_dir = job.dir,
+                                          volumes = volumes,
+                                          env = env,
+                                          container_id = container['id'],
+                                          group_mobyle = self.group_mobyle,
+                                          user_mobyle = self.user_mobyle,
+                                          container_type = container['type'],
+                                          )
+        return cmd
